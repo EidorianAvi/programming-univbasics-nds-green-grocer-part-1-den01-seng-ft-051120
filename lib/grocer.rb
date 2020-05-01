c70aaf102484a6e700803ccba8ce404cf29a2556
Rene Avila <reneavila1993@gmail.com>
2020-05-01 17:54:13 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,33 +2,35 @@ require 'pp'
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  index = 0
-  while index < collection.size do
-    item = collection[index]
-    if item[:item] == name
-      return item
-    end
-    index += 1
-  end
-  nil
+  counter = 0 
+  while counter < collection.length do
+    if collection[counter][:item] == name
+      return collection[counter]
+    end 
+  counter += 1 
+  end 
 end
 
 
 def consolidate_cart(cart)
-  consolidated = []
-  index = 0 
-  while index < cart.size do 
-    item = cart[index]
-    if (find_item_by_name_in_collection(item[:item], consolidated) == nil)
-      item[:count] = 1 
-    else 
-      item[:count] += 1 
+  new_cart = []
+  counter = 0 
+  while counter < cart.length do 
+    new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+    if new_cart_item
+      new_cart_item[:count] += 1 
+    else
+      new_cart_item = {
+        :item => cart[counter][:item],
+        :price => cart[counter][:price],
+        :clearance => cart[counter][:clearance],
+        :count => 1 
+      }
+      new_cart << new_cart_item
     end
-    consolidated << cart[index]
-  index += 1 
-  end
-  consolidated
+    counter += 1 
+  end 
+  new_cart
 end
-
 
   
